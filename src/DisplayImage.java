3767aaca74af7f0743051e5be1b578d0d4b3a004
Norht <charis.thron@gmx.de>
2021-05-29 20:42:58 +0200

    Imagges added

---
+++ src/DisplayImage.java
@@ -0,0 +1,77 @@
+
+public class DisplayImage {
+
+	
+	
+	
+	public static void Fehler (int Fehler) {
+		
+		if (Fehler == 0) {
+		
+		System.out.println(	"  +---+");
+		System.out.println( "  |   |");
+		System.out.println( "      |");
+		System.out.println( "      |");
+		System.out.println( "      |");
+		System.out.println( "      |");
+		System.out.println( "========= ");
+		}
+			if (Fehler == 1) {
+				System.out.println(	"  +---+");
+				System.out.println( "  |   |");
+				System.out.println( "  O   |");
+				System.out.println( "      |");
+				System.out.println( "      |");
+				System.out.println( "      |");
+				System.out.println( "========= ");
+			}
+		
+				if (Fehler == 2) {
+					System.out.println(	"  +---+");
+					System.out.println( "  |   |");
+					System.out.println( "  O   |");
+					System.out.println( "  |   |");
+					System.out.println( "      |");
+					System.out.println( "      |");
+					System.out.println( "========= ");
+				}
+					if (Fehler == 3) {
+						System.out.println(	"  +---+");
+						System.out.println( "  |   |");
+						System.out.println( "  O   |");
+						System.out.println( " /|   |");
+						System.out.println( "      |");
+						System.out.println( "      |");
+						System.out.println( "========= ");
+					}
+						if (Fehler == 4) {
+							System.out.println(	"  +---+");
+							System.out.println( "  |   |");
+							System.out.println( "  O   |");
+							System.out.println( " /|\\  |");
+							System.out.println( "      |");
+							System.out.println( "      |");
+							System.out.println( "========= ");
+						}
+							if (Fehler == 5) { 
+								System.out.println(	"  +---+");
+								System.out.println( "  |   |");
+								System.out.println( "  O   |");
+								System.out.println( " /|\\  |");
+								System.out.println( " /    |");
+								System.out.println( "      |");
+								System.out.println( "========= ");
+							}
+								if (Fehler == 6) {
+									System.out.println(	"  +---+");
+									System.out.println( "  |   |");
+									System.out.println( "  O   |");
+									System.out.println( " /|\\  |");
+									System.out.println( " / \\  |");
+									System.out.println( "      |");
+									System.out.println( "========= ");
+								}
+			
+	}
+}
+
--- src/Hangman.java
+++ src/Hangman.java
@@ -8,25 +8,28 @@ public class Hangman {
 	
 		System.out.println("Press enter to start");
 	
-	Scanner s = new Scanner(System.in);
-	s.nextLine();
-	
-	System.out.println("Game starts");
-	
-	String Wort = Wordlist.randomWord();
-	
-	int Länge = Wort.length(); 
-	
-	String nothing = "" ;
-	
-	for (int i=0; i<Länge; i++) {
-		nothing += "_ ";
+		Scanner s = new Scanner(System.in);
+		s.nextLine();
+		
+		System.out.println("Game starts");
+		
+		String Wort = Wordlist.randomWord();
+		
+		int Laenge = Wort.length(); 
+		
+		String nothing = "" ;
+		
+		for (int i=0; i<Laenge; i++) {
+			nothing += "_ ";
+		}
+		
+		System.out.println (nothing);
+		
+		DisplayImage.Fehler(0);
+		
 	}
-	System.out.println (nothing);
 	
-	}
-
-	
+		
 	
 	
 	
